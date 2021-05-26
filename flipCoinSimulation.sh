6e04c894e590d5bbdd69dc06020b5f607ff8da37
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-26 21:06:59 +0530

    calculating win

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -x
 echo "flip coin simulation"
+flips=1
 heads=0
 tails=0
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "heads is winner"
-	((heads++))
-else
-	echo "tails is winner"
-	((tails++))
-fi
+while [ $flips -le 100 ]
+do
+	random=$((RANDOM%3))
+	if [ $random -eq 1 ]
+	then
+		echo "heads is winner"
+		heads=$(($heads+1))
+		flips=$(($flips+1))
+	elif [ $random -eq 2 ]
+	then
+		echo "tails is winner"
+		tails=$(($tails+1))
+		flips=$(($flips+1))
+	fi
+done
+echo "head won $heads times and tails won $tails times"
+echo "total no.of flips are $flips"
