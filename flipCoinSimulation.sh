93d9f2fe52e326dc9de359fef2653e994daa06e6
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-26 23:18:51 +0530

    pushing win tie

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,19 +1,32 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "flip coin simulation"
 flips=1
 heads=0
 tails=0
+if [ $heads -gt $(($tails+2)) ]
+then
+        dif=$(($heads-$tails))
+        echo "Heads won by $dif times more than Tails"
+elif [ $tails -gt $(($heads+2)) ]
+then
+        dif=$(($tails-$heads))
+        echo "Tails won by $dif times more than Headss"
+else
+        echo "Its TIE since their difference is <=2"
+fi
+
+
 while [ $flips -le 100 ]
 do
 	random=$((RANDOM%3))
 	if [ $random -eq 1 ]
 	then
-		echo "heads is winner"
+		#echo "heads is winner"
 		heads=$(($heads+1))
 		flips=$(($flips+1))
 	elif [ $random -eq 2 ]
 	then
-		echo "tails is winner"
+		#echo "tails is winner"
 		tails=$(($tails+1))
 		flips=$(($flips+1))
 	fi
