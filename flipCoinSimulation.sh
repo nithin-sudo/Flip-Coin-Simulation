bdf38c4d68fed62052f6a38b3f4ea411903685d7
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-26 20:31:31 +0530

    adding winner

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 echo "flip coin simulation"
-
+heads=0
+tails=0
+random=$((RANDOM%2))
+if [ $random -eq 1 ]
+then
+	echo "heads is winner"
+	((heads++))
+else
+	echo "tails is winner"
+	((tails++))
+fi
